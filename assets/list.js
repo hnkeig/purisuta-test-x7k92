e9015da3105edb4d82a5dcd5b510543c3322db84
stmt <hnkeig@gmail.com>
2026-09-12 00:06:55 +0900

    女主人公

--- assets/list.js
+++ assets/list.js
@@ -7,12 +7,464 @@
 "use strict";
 /* ART_LIST_BEGIN */
 const ART_LIST = {
-  "chara": {},
-  "bg": [],
+  "chara": {
+    "aoi": {
+      "base": "",
+      "front": "",
+      "face": [],
+      "outfit": [],
+      "full": {
+        "": [
+          "normal.png"
+        ]
+      },
+      "bust": {
+        "": [
+          "ChatGPT Image 2026年9月3日 18_39_34.png_(384x512)_(2-1).png",
+          "ChatGPT Image 2026年9月3日 18_39_34.png_(384x512)_(2-4).png",
+          "blush.png",
+          "happy.png",
+          "s.png",
+          "sad.png",
+          "worry.png"
+        ]
+      },
+      "save": [],
+      "cg": []
+    },
+    "chikage": {
+      "base": "",
+      "front": "",
+      "face": [],
+      "outfit": [],
+      "full": {
+        "": [
+          "normal.png"
+        ]
+      },
+      "bust": {
+        "": [
+          "ChatGPT Image 2026年9月3日 22_47_13_r2_c1.png",
+          "angry.png",
+          "blush.png",
+          "happy.png",
+          "normal.png",
+          "sad.png",
+          "surprise.png",
+          "think.png"
+        ]
+      },
+      "save": [],
+      "cg": []
+    },
+    "daichi": {
+      "base": "",
+      "front": "",
+      "face": [],
+      "outfit": [],
+      "full": {
+        "": [
+          "normal.png"
+        ]
+      },
+      "bust": {
+        "": [
+          "ChatGPT Image 2026年9月4日 01_22_11.png_(384x512)_(1-1).png",
+          "ChatGPT Image 2026年9月4日 01_22_11.png_(384x512)_(1-2).png",
+          "ChatGPT Image 2026年9月4日 01_22_11.png_(384x512)_(1-3).png",
+          "ChatGPT Image 2026年9月4日 01_22_11.png_(384x512)_(2-1).png",
+          "ChatGPT Image 2026年9月4日 01_22_11.png_(384x512)_(2-3).png",
+          "ChatGPT Image 2026年9月4日 01_22_11.png_(384x512)_(2-4).png",
+          "ChatGPT Image 2026年9月4日 01_24_38.png_(384x512)_(1-1).png",
+          "ChatGPT Image 2026年9月4日 01_24_38.png_(384x512)_(1-2).png",
+          "ChatGPT Image 2026年9月4日 01_24_38.png_(384x512)_(1-4).png",
+          "ChatGPT Image 2026年9月4日 01_24_38.png_(384x512)_(2-4).png",
+          "ChatGPT Image 2026年9月4日 01_26_10.png_(443x443)_(1-1).png",
+          "ChatGPT Image 2026年9月4日 01_26_10.png_(443x443)_(1-3).png",
+          "ChatGPT Image 2026年9月4日 01_26_10.png_(443x443)_(2-1).png",
+          "ChatGPT Image 2026年9月4日 11_07_27.png",
+          "ChatGPT Image 2026年9月4日 11_07_27.png_(384x512)_(2-1).png",
+          "angry.png",
+          "blush.png",
+          "cry.png",
+          "happy.png",
+          "laugh.png",
+          "love.png",
+          "normal.png",
+          "sad.png",
+          "smile.png",
+          "think.png",
+          "worry.png"
+        ]
+      },
+      "save": [],
+      "cg": []
+    },
+    "hinata": {
+      "base": "",
+      "front": "",
+      "face": [],
+      "outfit": [],
+      "full": {
+        "": [
+          "normal.png"
+        ]
+      },
+      "bust": {
+        "": [
+          "ChatGPT Image 2026年9月4日 01_22_11.png_(384x512)_(1-1).png",
+          "ChatGPT Image 2026年9月4日 01_22_11.png_(384x512)_(1-2).png",
+          "ChatGPT Image 2026年9月4日 01_22_11.png_(384x512)_(1-3).png",
+          "ChatGPT Image 2026年9月4日 01_22_11.png_(384x512)_(2-1).png",
+          "ChatGPT Image 2026年9月4日 01_22_11.png_(384x512)_(2-3).png",
+          "ChatGPT Image 2026年9月4日 01_22_11.png_(384x512)_(2-4).png",
+          "ChatGPT Image 2026年9月4日 01_24_38.png_(384x512)_(1-1).png",
+          "ChatGPT Image 2026年9月4日 01_24_38.png_(384x512)_(1-4).png",
+          "ChatGPT Image 2026年9月4日 01_24_38.png_(384x512)_(2-4).png",
+          "ChatGPT Image 2026年9月4日 01_26_10.png_(443x443)_(1-1).png",
+          "ChatGPT Image 2026年9月4日 01_26_10.png_(443x443)_(1-3).png",
+          "ChatGPT Image 2026年9月4日 01_26_10.png_(443x443)_(2-1).png",
+          "angry.png",
+          "blush.png",
+          "cry.png",
+          "haeppy.png",
+          "happy.png",
+          "laugh.png",
+          "love.png",
+          "normal.png",
+          "sad.png",
+          "smile.png",
+          "think.png",
+          "worry.png"
+        ]
+      },
+      "save": [],
+      "cg": [
+        "01.png",
+        "02.png",
+        "03.png",
+        "04.png",
+        "05.png"
+      ]
+    },
+    "kanade": {
+      "base": "",
+      "front": "",
+      "face": [],
+      "outfit": [],
+      "full": {
+        "": [
+          "normal.png"
+        ]
+      },
+      "bust": {
+        "": [
+          "ChatGPT Image 2026年9月3日 18_39_34.png_(384x512)_(2-1).png",
+          "ChatGPT Image 2026年9月3日 18_39_34.png_(384x512)_(2-4).png",
+          "blush.png",
+          "happy.png",
+          "s.png",
+          "sad.png",
+          "worry.png"
+        ]
+      },
+      "save": [],
+      "cg": [
+        "01.png",
+        "02.png",
+        "03.png",
+        "04.png",
+        "05.png"
+      ]
+    },
+    "luka": {
+      "base": "",
+      "front": "",
+      "face": [],
+      "outfit": [],
+      "full": {
+        "": [
+          "normal.png"
+        ]
+      },
+      "bust": {
+        "": [
+          "ChatGPT Image 2026年9月4日 01_22_11.png_(384x512)_(1-1).png",
+          "ChatGPT Image 2026年9月4日 01_22_11.png_(384x512)_(1-2).png",
+          "ChatGPT Image 2026年9月4日 01_22_11.png_(384x512)_(1-3).png",
+          "ChatGPT Image 2026年9月4日 01_22_11.png_(384x512)_(2-1).png",
+          "ChatGPT Image 2026年9月4日 01_22_11.png_(384x512)_(2-3).png",
+          "ChatGPT Image 2026年9月4日 01_22_11.png_(384x512)_(2-4).png",
+          "ChatGPT Image 2026年9月4日 01_24_38.png_(384x512)_(1-1).png",
+          "ChatGPT Image 2026年9月4日 01_24_38.png_(384x512)_(1-2).png",
+          "ChatGPT Image 2026年9月4日 01_24_38.png_(384x512)_(1-4).png",
+          "ChatGPT Image 2026年9月4日 01_24_38.png_(384x512)_(2-4).png",
+          "ChatGPT Image 2026年9月4日 01_26_10.png_(443x443)_(1-1).png",
+          "ChatGPT Image 2026年9月4日 01_26_10.png_(443x443)_(1-3).png",
+          "ChatGPT Image 2026年9月4日 01_26_10.png_(443x443)_(2-1).png",
+          "ChatGPT Image 2026年9月4日 11_07_27.png",
+          "ChatGPT Image 2026年9月4日 11_07_27.png_(384x512)_(2-1).png",
+          "angry.png",
+          "blush.png",
+          "cry.png",
+          "happy.png",
+          "laugh.png",
+          "love.png",
+          "normal.png",
+          "sad.png",
+          "smile.png",
+          "think.png",
+          "worry.png"
+        ]
+      },
+      "save": [],
+      "cg": []
+    },
+    "minamo": {
+      "base": "",
+      "front": "",
+      "face": [],
+      "outfit": [],
+      "full": {
+        "": [
+          "normal.png"
+        ]
+      },
+      "bust": {
+        "": [
+          "ChatGPT Image 2026年9月4日 21_38_49.png_(443x443)_(1-1).png",
+          "ChatGPT Image 2026年9月4日 21_38_49.png_(443x443)_(1-4).png",
+          "ChatGPT Image 2026年9月4日 21_38_49.png_(443x443)_(2-1).png",
+          "ChatGPT Image 2026年9月4日 21_38_49.png_(443x443)_(2-2).png",
+          "ChatGPT Image 2026年9月4日 21_38_49.png_(443x443)_(2-3).png",
+          "ChatGPT Image 2026年9月4日 21_38_49.png_(443x443)_(2-4).png",
+          "ChatGPT Image 2026年9月4日 21_41_04.png_(443x443)_(1-2).png",
+          "ChatGPT Image 2026年9月4日 21_41_04.png_(443x443)_(1-3).png",
+          "ChatGPT Image 2026年9月4日 21_41_04.png_(443x443)_(2-1).png",
+          "ChatGPT Image 2026年9月4日 21_41_04.png_(443x443)_(2-2).png",
+          "ChatGPT Image 2026年9月4日 21_41_04.png_(443x443)_(2-4).png",
+          "blush.png",
+          "happy.png",
+          "normal.png",
+          "sad.png",
+          "worry.png"
+        ]
+      },
+      "save": [],
+      "cg": []
+    },
+    "nagisa": {
+      "base": "",
+      "front": "",
+      "face": [],
+      "outfit": [],
+      "full": {
+        "": [
+          "normal.png"
+        ]
+      },
+      "bust": {
+        "": [
+          "ChatGPT Image 2026年9月4日 01_22_11.png_(384x512)_(1-1).png",
+          "ChatGPT Image 2026年9月4日 01_22_11.png_(384x512)_(1-2).png",
+          "ChatGPT Image 2026年9月4日 01_22_11.png_(384x512)_(1-3).png",
+          "ChatGPT Image 2026年9月4日 01_22_11.png_(384x512)_(2-1).png",
+          "ChatGPT Image 2026年9月4日 01_22_11.png_(384x512)_(2-3).png",
+          "ChatGPT Image 2026年9月4日 01_22_11.png_(384x512)_(2-4).png",
+          "ChatGPT Image 2026年9月4日 01_24_38.png_(384x512)_(1-1).png",
+          "ChatGPT Image 2026年9月4日 01_24_38.png_(384x512)_(1-4).png",
+          "ChatGPT Image 2026年9月4日 01_24_38.png_(384x512)_(2-4).png",
+          "ChatGPT Image 2026年9月4日 01_26_10.png_(443x443)_(1-1).png",
+          "ChatGPT Image 2026年9月4日 01_26_10.png_(443x443)_(1-3).png",
+          "ChatGPT Image 2026年9月4日 01_26_10.png_(443x443)_(2-1).png",
+          "angry.png",
+          "blush.png",
+          "cry.png",
+          "haeppy.png",
+          "happy.png",
+          "laugh.png",
+          "love.png",
+          "normal.png",
+          "sad.png",
+          "smile.png",
+          "think.png",
+          "worry.png"
+        ]
+      },
+      "save": [],
+      "cg": [
+        "01.png",
+        "02.png",
+        "03.png",
+        "04.png",
+        "05.png"
+      ]
+    },
+    "rena": {
+      "base": "",
+      "front": "",
+      "face": [],
+      "outfit": [],
+      "full": {
+        "": [
+          "normal.png"
+        ]
+      },
+      "bust": {
+        "": [
+          "ChatGPT Image 2026年9月3日 22_47_13_r2_c1.png",
+          "angry.png",
+          "blush.png",
+          "happy.png",
+          "normal.png",
+          "sad.png",
+          "surprise.png",
+          "think.png"
+        ]
+      },
+      "save": [],
+      "cg": []
+    },
+    "ryu": {
+      "base": "",
+      "front": "",
+      "face": [],
+      "outfit": [],
+      "full": {
+        "": [
+          "normal.png"
+        ]
+      },
+      "bust": {
+        "": [
+          "ChatGPT Image 2026年9月4日 21_38_49.png_(443x443)_(1-1).png",
+          "ChatGPT Image 2026年9月4日 21_38_49.png_(443x443)_(1-4).png",
+          "ChatGPT Image 2026年9月4日 21_38_49.png_(443x443)_(2-1).png",
+          "ChatGPT Image 2026年9月4日 21_38_49.png_(443x443)_(2-2).png",
+          "ChatGPT Image 2026年9月4日 21_38_49.png_(443x443)_(2-3).png",
+          "ChatGPT Image 2026年9月4日 21_38_49.png_(443x443)_(2-4).png",
+          "ChatGPT Image 2026年9月4日 21_41_04.png_(443x443)_(1-2).png",
+          "ChatGPT Image 2026年9月4日 21_41_04.png_(443x443)_(1-3).png",
+          "ChatGPT Image 2026年9月4日 21_41_04.png_(443x443)_(2-1).png",
+          "ChatGPT Image 2026年9月4日 21_41_04.png_(443x443)_(2-2).png",
+          "ChatGPT Image 2026年9月4日 21_41_04.png_(443x443)_(2-4).png",
+          "blush.png",
+          "happy.png",
+          "normal.png",
+          "sad.png",
+          "worry.png"
+        ]
+      },
+      "save": [],
+      "cg": []
+    }
+  },
+  "bg": [
+    "amuse.png",
+    "amuse.webp",
+    "aqua.png",
+    "aqua.webp",
+    "artroom.png",
+    "artroom.webp",
+    "cafe.png",
+    "cafe.webp",
+    "fest.png",
+    "fest.webp",
+    "ground.png",
+    "ground.webp",
+    "home.png",
+    "home.webp",
+    "illum.png",
+    "illum.webp",
+    "klass.png",
+    "klass.webp",
+    "lib.png",
+    "lib.webp",
+    "momiji.png",
+    "momiji.webp",
+    "movie.png",
+    "movie.webp",
+    "music.png",
+    "music.webp",
+    "night.png",
+    "night.webp",
+    "park.png",
+    "park.webp",
+    "pet.png",
+    "pet.webp",
+    "room.png",
+    "room.webp",
+    "sakura.png",
+    "sakura.webp",
+    "school.png",
+    "school.webp",
+    "sea.png",
+    "sea.webp",
+    "shrine.png",
+    "shrine.webp",
+    "skate.png",
+    "skate.webp",
+    "sunset.png",
+    "sunset.webp",
+    "title_autumn.png",
+    "title_autumn.webp",
+    "title_spring.png",
+    "title_spring.webp",
+    "title_summer.png",
+    "title_summer.webp",
+    "title_winter.png",
+    "title_winter.webp",
+    "town.png",
+    "town.webp"
+  ],
   "cg": [],
-  "bgm": [],
+  "bgm": [
+    "autumn.mp3",
+    "spring.mp3",
+    "summer.mp3",
+    "title.mp3",
+    "vspring.mp3",
+    "vsummer.mp3",
+    "vwinter.mp3",
+    "winter.mp3"
+  ],
   "se": [],
-  "ui": [],
-  "font": []
+  "ui": [
+    "logo.png",
+    "ui_btn_auto_hover.png",
+    "ui_btn_auto_normal.png",
+    "ui_btn_load_hover.png",
+    "ui_btn_load_normal.png",
+    "ui_btn_menu_hover.png",
+    "ui_btn_menu_normal.png",
+    "ui_btn_save_hover.png",
+    "ui_btn_save_normal.png",
+    "ui_btn_skip_hover.png",
+    "ui_btn_skip_normal.png",
+    "ui_dialogue_panel.png",
+    "ui_nameplate.png",
+    "ui_photo_frame.png",
+    "ui_photo_mask.png",
+    "ui_title_arrow_right.png",
+    "ui_title_base_continue_hover.png",
+    "ui_title_base_continue_normal.png",
+    "ui_title_base_extra_hover.png",
+    "ui_title_base_extra_normal.png",
+    "ui_title_base_newgame_hover.png",
+    "ui_title_base_newgame_normal.png",
+    "ui_title_base_option_hover.png",
+    "ui_title_base_option_normal.png",
+    "ui_title_icon_continue.png",
+    "ui_title_icon_extra.png",
+    "ui_title_icon_newgame.png",
+    "ui_title_icon_option.png",
+    "ui_title_label_continue_ja.png",
+    "ui_title_label_extra_ja.png",
+    "ui_title_label_newgame_ja.png",
+    "ui_title_label_option_ja.png"
+  ],
+  "font": [
+    "rounded-mplus-2p-medium.ttf"
+  ]
 };
 /* ART_LIST_END */
